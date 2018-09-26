7c425a1d8a5d16fb08d052c3cbeb4bcaf3285eca
Artyom Y. Razinov <arazinov@avito.ru>
2018-09-26 16:44:28 +0300

    Play with image hashing

--- Tests/XcuiTests/Tests/ScreenshotTests.swift
+++ Tests/XcuiTests/Tests/ScreenshotTests.swift
@@ -2,6 +2,7 @@ import MixboxUiTestsFoundation
 import MixboxIpc
 import XCTest
 import MixboxFoundation
+import CocoaImageHashing
 
 class ScreenshotTests: TestCase {
     func test() {
@@ -20,6 +21,28 @@ class ScreenshotTests: TestCase {
             }
             pageObjects.screenXcui.view(index: index).withoutTimeout.assert.matchesReference(image: image)
             pageObjects.screen.view(index: index).withoutTimeout.assert.matchesReference(image: image)
+        }
+    }
+    
+    func test_1() {
+        let hashing = OSImageHashing.sharedInstance()
+        
+        let images = ["cat_size", "cat_text", "cat_color", "cat", "cat_aspect", "cat_borders", "cat_not_cat"]
+        
+        print("-\t\(images.joined(separator: "\t"))")
+        
+        for a in images {
+            print("\(a)", terminator: "")
+            for b in images {
+                let image0 = UIImage(named: a, in: Bundle(for: type(of: self)), compatibleWith: nil)!
+                let image1 = UIImage(named: b, in: Bundle(for: type(of: self)), compatibleWith: nil)!
+                let dist = hashing.hashDistance(
+                    hashing.hashImage(image0, with: .aHash),
+                    to: hashing.hashImage(image1, with: .aHash)
+                )
+                print("\t\(dist)", terminator: "")
+            }
+            print()
         }
     }
 }
